7488e25166a03e736ded014bd28efa38d0b68d12
Arturo Campos <arturplur@gmail.com>
2020-01-29 22:45:37 -0500

    Request all repository content

--- map.r
+++ map.r
@@ -75,10 +75,55 @@ fn_names <- function(x){
     
   }}
 nms <- map(org_count, fn_names)
-nms
+
 
 ############
-# 
+# List project names
+org_count <- length(nms)
+
+fn_repo_projs <- function(x){
+   nms[1:x]
+
+}
+repo_projs_list <- map(org_count, fn_repo_projs)
+#repo_projs_list <- repo_projs_list[[1]]
+#repo_projs_list[[2]]
 
 
+############
+#Count projects from project list
+#It is really unnecessary, use "org_count" instead.
+fn_count_projs <- function(x){
+  length(repo_projs_list[[1]][[x]])
+}
+count_projs <- map(1:org_count, fn_count_projs)
+#str(count_projs)
 
+############
+
+#Request all repo content.
+#GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=5.1
+fn_repos <- function(x, y){
+  if(!require(httr)){
+    stop('httr not installed')
+  } else {
+    
+    GET(
+     paste0(
+        "https://dev.azure.com/",
+        x,
+        "/",
+        y,
+        "/_apis/git/repositories?api-version=5.1"
+      ),
+      authenticate(user = az_pat, password = az_pat)
+    )
+  }
+}
+
+#https://purrr.tidyverse.org/reference/map2.html
+az_dev_repos_urls <- map2(.x = rep(orgs, count_projs) , .y = unlist(repo_projs_list[[1]]), .f = fn_repos)
+
+#typeof(az_dev_repos_urls)
+
+#str(az_dev_repos_urls)
