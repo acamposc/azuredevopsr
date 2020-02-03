69481140af6912670b05cbfd105e4a64e51be76b
Arturo Campos <arturplur@gmail.com>
2020-02-03 17:24:56 -0500

    path to ga auth

--- map_ks.r
+++ map_ks.r
@@ -6,4 +6,4 @@ GOOGLE_BIGQUERY_DATASET_NAME <- read.table(file = "GOOGLE_BIGQUERY_DATASET_NAME.
 #GCS_DEFAULT_BUCKET <- read.table(file = "GCS_DEFAULT_BUCKET.txt")
 #CR_REGION <- read.table(file = "CR_REGION.txt")
 #CR_BUILD_EMAIL <- read.table(file = "CR_BUILD_EMAIL.txt")
-GOOGLE_SERVICE_ACCOUNT_PATH <- c("/attach-commits-fa50e59d273d.json")
+GOOGLE_SERVICE_ACCOUNT_PATH <- c("attach-commits-fa50e59d273d.json") 
