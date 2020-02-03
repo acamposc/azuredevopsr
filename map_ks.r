b2dbef7236c61a09f53dc90e525229e8af3a59be
Arturo Campos <arturplur@gmail.com>
2020-02-03 16:07:22 -0500

    Execution halted   cannot open file '/AZURE_PAT.txt': No such file or directory

--- map_ks.r
+++ map_ks.r
@@ -1,9 +1,9 @@
 
-AZURE_PAT <- read.table(file = "/AZURE_PAT.txt")
-GOOGLE_CLOUD_PROJECT_ID <- read.table(file = "/GOOGLE_CLOUD_PROJECT_ID.txt")
-GOOGLE_BIGQUERY_TABLE_NAME <- read.table(file = "/GOOGLE_BIGQUERY_TABLE_NAME.txt")
-GOOGLE_BIGQUERY_DATASET_NAME <- read.table(file = "/GOOGLE_BIGQUERY_DATASET_NAME.TXT")
-GCS_DEFAULT_BUCKET <- read.table(file = "/GCS_DEFAULT_BUCKET.txt")
-CR_REGION <- read.table(file = "/CR_REGION.txt")
-CR_BUILD_EMAIL <- read.table(file = "/CR_BUILD_EMAIL.txt")
-GOOGLE_SERVICE_ACCOUNT_PATH <- "/attach-commits-fa50e59d273d.json"
+AZURE_PAT <- read.table(file = "AZURE_PAT.txt")
+GOOGLE_CLOUD_PROJECT_ID <- read.table(file = "GOOGLE_CLOUD_PROJECT_ID.txt")
+GOOGLE_BIGQUERY_TABLE_NAME <- read.table(file = "GOOGLE_BIGQUERY_TABLE_NAME.txt")
+GOOGLE_BIGQUERY_DATASET_NAME <- read.table(file = "GOOGLE_BIGQUERY_DATASET_NAME.TXT")
+GCS_DEFAULT_BUCKET <- read.table(file = "GCS_DEFAULT_BUCKET.txt")
+CR_REGION <- read.table(file = "CR_REGION.txt")
+CR_BUILD_EMAIL <- read.table(file = "CR_BUILD_EMAIL.txt")
+GOOGLE_SERVICE_ACCOUNT_PATH <- "attach-commits-fa50e59d273d.json"
